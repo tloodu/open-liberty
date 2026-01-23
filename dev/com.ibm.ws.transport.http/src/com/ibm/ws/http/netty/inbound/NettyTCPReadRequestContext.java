dcc032ca91f6e328690d8cd1ded5cda9aa945fb7
Manuel Saldana <mansal@us.ibm.com>
2026-01-23 17:38:06 -0500

    Netty ReadOnlySocket API test

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/inbound/NettyTCPReadRequestContext.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/inbound/NettyTCPReadRequestContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 IBM Corporation and others.
+ * Copyright (c) 2023, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -94,11 +94,9 @@ public class NettyTCPReadRequestContext implements TCPReadRequestContext {
     @Override
     public Socket getSocket() {
         if(cachedSocket == null){
-            cachedSocket = new ReadOnlySocket(nettyChannel);
-            
+            cachedSocket = new ReadOnlySocket(nettyChannel);       
         }
         return cachedSocket;
-        //throw new UnsupportedOperationException("Can not get the socket from a Netty connection!");
     }
 
     /**
