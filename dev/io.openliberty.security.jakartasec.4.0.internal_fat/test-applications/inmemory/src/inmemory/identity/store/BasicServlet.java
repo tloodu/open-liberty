628b96ddb1bf112d65d66635e7a04709ab72ea0f
theodore-gkoumas <theodore.gkoumas@ibm.com>
2026-01-26 10:25:26 +0000

    Adding the new annotation to be tested on servlet

--- dev/io.openliberty.security.jakartasec.4.0.internal_fat/test-applications/inmemory/src/inmemory/identity/store/BasicServlet.java
+++ dev/io.openliberty.security.jakartasec.4.0.internal_fat/test-applications/inmemory/src/inmemory/identity/store/BasicServlet.java
@@ -9,16 +9,36 @@
  *******************************************************************************/
 package inmemory.identity.store;
 
+import static jakarta.security.enterprise.identitystore.IdentityStore.ValidationType.PROVIDE_GROUPS;
+import static jakarta.security.enterprise.identitystore.IdentityStore.ValidationType.VALIDATE;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import componenttest.app.FATServlet;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.identitystore.InMemoryIdentityStoreDefinition;
+import jakarta.security.enterprise.identitystore.InMemoryIdentityStoreDefinition.Credentials;
 import jakarta.servlet.annotation.WebServlet;
 
-/*
- *
- */
+@BasicAuthenticationMechanismDefinition(realmName = "basicAuth")
+@InMemoryIdentityStoreDefinition(
+                                 priority = 10,
+                                 priorityExpression = "${80/20}",
+                                 useFor = { VALIDATE, PROVIDE_GROUPS },
+                                 //useForExpression = "#{'VALIDATE'}",
+                                 value = {
+                                           @Credentials(callerName = "jasmine", password = "secret1", groups = { "caller", "user" }),
+                                           @Credentials(callerName = "lisa", password = "{xor}LDo8LTorbg==", groups = { "caller", "user" }),
+                                           // not in any valid groups
+                                           @Credentials(callerName = "bill", password = "{xor}LDo8LTorbg==", groups = { "foo", "bar" }),
+                                           // bad {xor} string leads to decoding error to test error message output
+                                           @Credentials(callerName = "johnny", password = "{xor}LLTxlkwjljsdforbg=", groups = { "foo", "bar" }),
+                                           @Credentials(callerName = "frank", groups = { "user" },
+                                                        password = "{hash}ARAAAAAUUEJLREYyV2l0aEhtYWNTSEE1MTIwAAAAIKJLaCvuDfiQYK8H/6SWdTzmbMxndGqWyUWnCaA3ZPOZQAAAACBnjkRtcavFNC2k2qbwEitLlmHZKTYRmeuuCh8z1nFyyw=="),
+                                           @Credentials(callerName = "sally", groups = { "user" },
+                                                        password = "{aes}ARAFCrWIYJCL7ZBNjN+MKcJoozBmbZyJPood6X6sCqMIRBaouZSd4B0u0Gcgdp/tXWwuOwi/mqYLS0cGTwuU95tgP4Y+6hgtvG2ST2gT+ghTPhLJWfiXTrvBRvR5yf2F9hkmM7SG/WRQNA==")
+                                 })
 @WebServlet("/BasicServlet")
 public class BasicServlet extends FATServlet {
     private static final long serialVersionUID = 1L;
