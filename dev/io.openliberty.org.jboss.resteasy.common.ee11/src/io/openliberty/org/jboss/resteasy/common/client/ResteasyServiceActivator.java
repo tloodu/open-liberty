c77da8707c05387da5f88545a66a3ebe39bfe0e4
Neena Jacob <neena.jacob@ibm.com>
2026-02-17 20:57:38 +0530

    Updates to make RuntimeDelegate available without adding the property

---
+++ dev/io.openliberty.org.jboss.resteasy.common.ee11/src/io/openliberty/org/jboss/resteasy/common/client/ResteasyServiceActivator.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.org.jboss.resteasy.common.client;
+
+import jakarta.ws.rs.ext.RuntimeDelegate;
+
+import org.jboss.resteasy.core.providerfactory.ResteasyProviderFactoryImpl;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * ResteasyServiceActivator is used to initialize global configurations for RESTEasy.
+ * This activator ensures early initialization of the RuntimeDelegate
+ * 
+ */
+@Component(immediate = true, property = { "service.vendor=IBM" }, configurationPolicy = ConfigurationPolicy.OPTIONAL)
+public class ResteasyServiceActivator {
+
+    /**
+     * Called by Declarative Services to activate service.
+     */
+    @Activate
+    protected void activate(ComponentContext cc) {
+        RuntimeDelegate.setInstance(new ResteasyProviderFactoryImpl());
+    }
+
+    /**
+     * Called by Declarative Services to deactivate service.
+     */
+    @Deactivate
+    protected void deactivate(ComponentContext cc) {
+        RuntimeDelegate.setInstance(null);
+    }
+}
