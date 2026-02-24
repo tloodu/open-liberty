d88e1e1287eabaeec69f88f41d9c1f0753d09a93
Paul Nicolucci <pnicolucci@gmail.com>
2026-02-24 18:44:56 -0500

    Code review updates

--- dev/com.ibm.ws.jpa.tests.container.client_fat/fat/src/com/ibm/ws/jpa/clientcontainer/fat/FATSuite.java
+++ dev/com.ibm.ws.jpa.tests.container.client_fat/fat/src/com/ibm/ws/jpa/clientcontainer/fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2026 IBM Corporation and others.
+ * Copyright (c) 2021, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ public class FATSuite {
     @ClassRule
     public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.NO_REPLACEMENT().fullFATOnly())
                     .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
-                    .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                    .andWith(FeatureReplacementAction.EE10_FEATURES())
                     .andWith(FeatureReplacementAction.EE11_FEATURES());
 //    @ClassRule
 //    public static ExternalResource testRule = new ExternalResource() {
