c9bf6f82f38805fa0ab40f150b684e3e24e6bfea
theodore-gkoumas <theodore.gkoumas@ibm.com>
2026-03-24 11:14:34 +0000

    testing multiple id stores in the same binary

--- dev/io.openliberty.security.jakartasec.4.0.internal_fat/fat/src/io/openliberty/security/jakartasec/fat/FATSuite.java
+++ dev/io.openliberty.security.jakartasec.4.0.internal_fat/fat/src/io/openliberty/security/jakartasec/fat/FATSuite.java
@@ -16,42 +16,27 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.custom.junit.runner.AlwaysPassesTest;
-import io.openliberty.security.jakartasec.fat.tests.AppBndRolesTests;
-import io.openliberty.security.jakartasec.fat.tests.AppRolesTests;
-import io.openliberty.security.jakartasec.fat.tests.HAMWithInBuiltTests;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdStoreAesEncodedPwdTests;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdStoreBadlyEncodedPwdTests;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdentityStoreELWarningTest;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdentityStoreEnablementTests;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdentityStorePropertyNotFoundTest;
-import io.openliberty.security.jakartasec.fat.tests.InMemoryIdentityStoreTests;
-import io.openliberty.security.jakartasec.fat.tests.MissingCustomHandlerTests;
-import io.openliberty.security.jakartasec.fat.tests.MultipleHAMCustomTests;
-import io.openliberty.security.jakartasec.fat.tests.MultipleHAMDuplicateTests;
-import io.openliberty.security.jakartasec.fat.tests.MultipleHAMInbuiltQualifiersTests;
-import io.openliberty.security.jakartasec.fat.tests.MultipleHAMInbuiltTests;
-import io.openliberty.security.jakartasec.fat.tests.SingleHAMInbuiltCustomQualifierTests;
+import io.openliberty.security.jakartasec.fat.tests.MultipleInMemoryIdentityStoresTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-                AlwaysPassesTest.class,
-                MultipleHAMCustomTests.class,
-                MultipleHAMDuplicateTests.class,
-                MultipleHAMInbuiltTests.class,
-                MultipleHAMInbuiltQualifiersTests.class,
-                HAMWithInBuiltTests.class,
-                SingleHAMInbuiltCustomQualifierTests.class,
-                MissingCustomHandlerTests.class,
-                InMemoryIdentityStoreTests.class,
-                InMemoryIdentityStoreELWarningTest.class,
-                InMemoryIdentityStorePropertyNotFoundTest.class,
-                InMemoryIdentityStoreEnablementTests.class,
-                AppRolesTests.class,
-                AppBndRolesTests.class,
-                InMemoryIdStoreBadlyEncodedPwdTests.class,
-                InMemoryIdStoreBadlyEncodedPwdTests.class,
-                InMemoryIdStoreAesEncodedPwdTests.class
+//                AlwaysPassesTest.class,
+//                MultipleHAMCustomTests.class,
+//                MultipleHAMDuplicateTests.class,
+//                MultipleHAMInbuiltTests.class,
+//                MultipleHAMInbuiltQualifiersTests.class,
+//                HAMWithInBuiltTests.class,
+//                SingleHAMInbuiltCustomQualifierTests.class,
+//                MissingCustomHandlerTests.class,
+//                InMemoryIdentityStoreTests.class,
+//                InMemoryIdentityStoreELWarningTest.class,
+//                InMemoryIdentityStorePropertyNotFoundTest.class,
+//                InMemoryIdentityStoreEnablementTests.class,
+//                InMemoryIdStoreBadlyEncodedPwdTests.class,
+//                InMemoryIdStoreAesEncodedPwdTests.class,
+                MultipleInMemoryIdentityStoresTests.class,
+//                AppRolesTests.class,
+//                AppBndRolesTests.class
 })
 public class FATSuite {
 }
